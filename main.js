0092570d1e87120043023fe16352fce80ac37251
Kayathri2218 <kaya3kayu2000@gmail.com>
2023-02-06 15:20:01 -0500

    adding javascript file reduce method

---
+++ main.js
@@ -0,0 +1,17 @@
+// Reduce Method
+
+document.write('<br>');
+document.write('<br>');
+
+document.write("6. Reduce Method");
+
+document.write('<br>');
+
+const numbers = [1, 2, 3, 4, 5];
+const out = numbers.reduce((v1, v2) => v1 * v2);
+total = out / 3;
+console.log(total);
+document.write(total);
+
+document.write('<br>');
+document.write('<br>');
